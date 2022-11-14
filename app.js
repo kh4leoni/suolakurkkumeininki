726021ab4e84657cea28da0c7a58c556974d6726
kh4leoni <73070339+kh4leoni@users.noreply.github.com>
2022-11-14 22:49:52 +0200

    Lisätty navbar burgeriin

---
+++ app.js
@@ -0,0 +1,10 @@
+const menuButton = document.getElementsByClassName('toggle-button')[0];
+const navLink = document.getElementsByClassName('nav-link-div')[0];
+
+
+menuButton.addEventListener('click', () => {
+  navLink.classList.toggle('active')
+}) 
+ 
+
+  
