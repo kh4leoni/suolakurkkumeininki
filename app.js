df155ad34e1ade02b35a10115a085bb97661d66f
kh4leoni <73070339+kh4leoni@users.noreply.github.com>
2022-11-15 20:54:07 +0200

    Lisätty toimiva mob nav uusi resepti sivulle

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const menuButton = document.getElementsByClassName('toggle-button')[0];
-const navLink = document.getElementsByClassName('nav-link-div')[0];
+const menuButton = document.getElementsByClassName("toggle-button")[0];
+const navLink = document.getElementsByClassName("nav-link-div")[0];
+const newNavLink = document.getElementsByClassName("new-navi-div")[0];
 
-
-menuButton.addEventListener('click', () => {
-  navLink.classList.toggle('active')
-}) 
- 
-
-  
+menuButton.addEventListener("click", () => {
+  if (navLink) {
+    navLink.classList.toggle("active");
+  } else if (newNavLink) {
+    newNavLink.classList.toggle("active");
+    console.log(newNavLink);
+  }
+});
