01c7edbc37df9451dbb74b3beb5907f545869c16
kh4leoni <73070339+kh4leoni@users.noreply.github.com>
2022-11-21 00:28:27 +0200

    Lisätty arvostelujen lisäys ominaisuus

--- arvostelut.js
+++ arvostelut.js
@@ -1,56 +1,120 @@
-const backgroundImage = document.querySelectorAll('.kurkku')[0];
+const backgroundImage = document.querySelectorAll(".kurkku")[0];
 let profileOpen = false;
-const wrapper = document.querySelectorAll('.wrapper')[0];
+const wrapper = document.querySelectorAll(".wrapper")[0];
 
-const reviewBtn = document.getElementById('review-btn');
-const reviewCard = document.getElementById('review-card');
-const leftSide = document.getElementById('left-side');
+const reviewBtn = document.getElementById("review-btn");
+const reviewCard = document.getElementById("review-card");
+const leftSide = document.getElementById("left-side");
 
-reviewBtn.addEventListener('click', () => {
-  reviewCard.classList.toggle('active');
-  const nappi = leftSide.classList.toggle('active');   
-  
-})
+const pickleName = document.getElementById("name-field");
+const pickleType = document.getElementById("type-field");
+const pickleFlavor = document.getElementById("customRange2");
+const pickleTexture = document.getElementById("customRange3");
+const submitReviewBtn = document.getElementById("submit-review-btn");
+let uploadedImage;
+
+reviewBtn.addEventListener("click", () => {
+  reviewCard.classList.toggle("active");
+ 
+});
+
+
 
 
 let data = [
-  { id: 1, name: 'Villen Rapsakat', img: './images/villen_rapsakat.png', tyyli: 'Venäläinen suolakurkku', arvosana: 1.5, maku: 40, suuTuntuma: 10 },
-  { id: 2, name: 'Euro East', img: './images/euro-east-suolakurkku.jpg', tyyli: 'Lohkottu suolakurkku', arvosana: 3, maku: 71, suuTuntuma: 77 },
-  { id: 3, name: 'Myrttinen', img: './images/myrttisen-valkosipulikurkut.jpg', tyyli: 'Valkosipulisuolakurkut', arvosana: 5, maku: 100, suuTuntuma: 100 },
-  { id: 1, name: 'Villen Rapsakat', img: './images/villen_rapsakat.png', tyyli: 'Venäläinen suolakurkku', arvosana: 1.5, maku: 40, suuTuntuma: 10 },
-  { id: 2, name: 'Euro East', img: './images/euro-east-suolakurkku.jpg', tyyli: 'Lohkottu suolakurkku', arvosana: 3, maku: 71, suuTuntuma: 77 },
-  { id: 3, name: 'Myrttinen', img: './images/myrttisen-valkosipulikurkut.jpg', tyyli: 'Valkosipulisuolakurkut', arvosana: 5, maku: 100, suuTuntuma: 100 },
-  { id: 2, name: 'Euro East', img: './images/euro-east-suolakurkku.jpg', tyyli: 'Lohkottu suolakurkku', arvosana: 3, maku: 71, suuTuntuma: 77 },
-  { id: 3, name: 'Myrttinen', img: './images/myrttisen-valkosipulikurkut.jpg', tyyli: 'Valkosipulisuolakurkut', arvosana: 5, maku: 100, suuTuntuma: 100 },
+  {
+    id: 1,
+    name: "Villen Rapsakat",
+    img: "./images/villen_rapsakat.png",
+    tyyli: "Venäläinen suolakurkku",
+    maku: 2,
+    suuTuntuma: 1,
+  },
+  {
+    id: 2,
+    name: "Euro East",
+    img: "./images/euro-east-suolakurkku.jpg",
+    tyyli: "Lohkottu suolakurkku",
+    maku: 3,
+    suuTuntuma: 2,
+  },
+  {
+    id: 3,
+    name: "Myrttinen",
+    img: "./images/myrttisen-valkosipulikurkut.jpg",
+    tyyli: "Valkosipulisuolakurkut",
+    maku: 5,
+    suuTuntuma: 5,
+  },
+  {
+    id: 4,
+    name: "Villen Rapsakat",
+    img: "./images/villen_rapsakat.png",
+    tyyli: "Venäläinen suolakurkku",
+    maku: 2,
+    suuTuntuma: 1,
+  },
+  {
+    id: 5,
+    name: "Euro East",
+    img: "./images/euro-east-suolakurkku.jpg",
+    tyyli: "Lohkottu suolakurkku",
+    maku: 3,
+    suuTuntuma: 3,
+  },
+];
 
-]
+const savedData = JSON.parse(localStorage.getItem('data'))
+data = savedData;
 
 
 
-let cards = " ";
 
-data.forEach((item) => {
-  cards += `
+renderReviews();
+
+submitReviewBtn.addEventListener("click", () => {
+  addNewReview();
+
+  // reviewCard.innerHTML =
+  // "<h1>Kiitos arvostelusta!</h1><button class='btn text-white' id='write-new-btn'>Kirjoita uusi</button>";
+  // const writeNew = document.getElementById("write-new-btn");
+  // if (writeNew) {
+  //   writeNew.addEventListener("click", () => {
+  //     // window.location.reload();
+  //   });
+  // }
+});
+
+function renderReviews() {
+  let cards = " ";
+
+  data.forEach((item) => {
+    cards += `
   <div class="col-12 col-md-6 col-lg-4"s>
       <div class="card">
           <img class="w-100" src="${item.img}" alt="">
           <div class="card-body">
               <div class="d-flex justify-content-between">
-                  <h4 class="card-title" style="font-size: 15px;font-weight:600;">${item.name}</h4>
-                  <h4 style="font-weight:600;font-size: 15px;"><i class="fa-solid fa-star text-warning mx-2"></i>${item.arvosana} / 5 </h4>
+                  <h4 class="card-title" style="font-size: 15px;font-weight:600;">${
+                    item.name
+                  }</h4>
+                  <h4 style="font-weight:600;font-size: 15px;"><i class="fa-solid fa-star text-warning mx-2"></i>${
+                    (item.maku + item.suuTuntuma) / 2
+                  } / 5 </h4>
               </div>
               <p class="card-text">${item.tyyli}</p>
-              <button class="btn btn-primary w-100 open-button" onclick="profile(${item.id})">Suurenna</button>
+              <button class="btn btn-primary w-100 open-button" onclick="profile(${
+                item.id
+              })">Suurenna</button>
           </div>
       </div>
   </div>
   `;
-});
 
-
-
-const panel = document.getElementById("panel");
-panel.innerHTML = cards;
+    const panel = document.getElementById("panel");
+    panel.innerHTML = cards;
+  });
+}
 const profileCard = document.getElementById("profile");
 
 function profile(id) {
@@ -59,11 +123,11 @@ function profile(id) {
   profileCard.style.opacity = 1;
   panel.style.opacity = 0.5;
   backgroundImage.style.transform = "scale(1)";
-    profileOpen = true;
- 
+  profileOpen = true;
+
   data.forEach((item) => {
-      if (item.id === id) {
-          let profile_div = `
+    if (item.id === id) {
+      let profile_div = `
               <div class="w-100 small-screen-margin">
                   <span id="close-button" class="close float-end" onclick="closeProfile()">&times</span>
               </div>
@@ -71,52 +135,88 @@ function profile(id) {
 
   <img class="w-100" src="${item.img}" alt="" id="profile-img">
   <div class="">
-      <h2 style="font-size: 20px;" class="display-6 text-center small-profile">${item.name}</h4>
+      <h2 style="font-size: 20px;" class="display-6 text-center small-profile">${
+        item.name
+      }</h4>
       <p class="card-text text-center">(${item.tyyli})</p>
   </div>
 </div>
 <div class="col-md-9">
-  <h3 class="display-6 mt-2" style="font-size: 30px;letter-spacing:1.5px;">ARVOSANA</h3><span>${item.arvosana} / 5</span>
+  <h3 class="display-6 mt-2" style="font-size: 30px;letter-spacing:1.5px;">ARVOSANA</h3><span>${
+    (item.maku + item.suuTuntuma) / 2
+  } / 5</span>
   <div class="progress">
-      <div class="progress-bar" style="width:${(item.arvosana / 5) * 100}%"></div>
+      <div class="progress-bar" style="width:${
+        ((item.maku + item.suuTuntuma) / 2 / 5) * 100
+      }%"></div>
   </div>
   <h2 class="display-6 mt-2" style="font-size: 30px;letter-spacing:1.5px;">MAKU</h2>
-  <span>${item.maku} / 100</span>
+  <span>${item.maku} / 5</span>
   <div class="progress">
-      <div class="progress-bar" style="width:${(item.maku / 100) * 100}%"></div>
+      <div class="progress-bar" style="width:${(item.maku / 5) * 100}%"></div>
   </div>
   <h2 class="display-6 my-2" style="font-size: 30px;letter-spacing:1.5px;">SUUTUNTUMA</h2>
-  <span>${item.suuTuntuma} / 100</span>
+  <span>${item.suuTuntuma} / 5</span>
   <div class="progress">
-      <div class="progress-bar" style="width:${(item.suuTuntuma / 100 ) * 100}%"></div>
+      <div class="progress-bar" style="width:${
+        (item.suuTuntuma / 5) * 100
+      }%"></div>
   </div>
 </div>
       `;
 
-            
-          profileCard.innerHTML = profile_div;
-        //   const openBtn = document.querySelectorAll('.open-button')[0];
-        //     openBtn.addEventListener('click', (e) => {
-        //         console.log(e)
-        //     })
+      profileCard.innerHTML = profile_div;
+      //   const openBtn = document.querySelectorAll('.open-button')[0];
+      //     openBtn.addEventListener('click', (e) => {
+      //         console.log(e)
+      //     })
 
-            
-        //   wrapper.addEventListener('click', () => {
-        //     if(profileOpen && !profile()) {
-        //         closeProfile();
-        //     }
-        //   })
-
-      }
-  })
-
+      //   wrapper.addEventListener('click', () => {
+      //     if(profileOpen && !profile()) {
+      //         closeProfile();
+      //     }
+      //   })
+    }
+  });
 }
 
-
-
-function closeProfile(){
+function closeProfile() {
   profileCard.style.transform = "scale(0)";
   profileCard.style.opacity = 0;
-  panel.style.opacity = 1; 
+  panel.style.opacity = 1;
+}
 
+const image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function () {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    uploadedImage = reader.result;
+
+    document.querySelector(
+      ".img-placeholder"
+    ).style.backgroundImage = `url(${uploadedImage})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
+
+function addNewReview() {
+  let newId = data.length + 1;
+  const newPickle = {
+    id: newId,
+    name: pickleName.value,
+    img: uploadedImage,
+    tyyli: pickleType.value,
+    maku: parseInt(pickleFlavor.value),
+    suuTuntuma: parseInt(pickleTexture.value),
+  };
+
+  if (pickleName.value != "" && pickleType.value != "") {
+    data.push(newPickle);
+    console.log(data)
+    localStorage.setItem("data", JSON.stringify(data));
+    renderReviews();
+    newId++;
+  } else {
+    alert("Täytä molemmat kentät");
+  }
 }
